7e332307a870998d9731becad3686bd3104eac97
RlxHomie <131203376+RlxHomie@users.noreply.github.com>
2025-08-05 18:20:49 +0200

    Update env.js

--- public/env.js
+++ public/env.js
@@ -2,5 +2,6 @@
 window._env_ = {
   AZURE_CLIENT_ID : 'b5c6f963-3def-4d73-ac74-6bbf6cf37a62',
   AZURE_TENANT_ID : 'a70783e2-cf58-4e38-bfd7-b403c7c833af',
-  EXCEL_FILE_ID   : '01WYAE7MT4WASPPHSYWVGLEAAYVUZCF2O5'
+  EXCEL_FILE_ID   : '01WYAE7MT4WASPPHSYWVGLEAAYVUZCF2O5',
+   NODE_ENV        : 'production'      // «development» en local si quieres
 };
